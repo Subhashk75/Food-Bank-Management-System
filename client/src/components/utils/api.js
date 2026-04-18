c76386bfe63b72924696edf2046a828d9b87ab27
subhsh <kumawatsubhash388@gmail.com>
2026-04-18 10:43:58 +0530

    put live url on frontend

--- client/src/components/utils/api.js
+++ client/src/components/utils/api.js
@@ -1,4 +1,4 @@
-export const API_BASE = 'http://localhost:3000/api/v1';
+export const API_BASE = 'https://food-bank-management-system-1.onrender.com//api/v1';
 
 const apiRequest = async (endpoint, method, data = null) => {
   const url = `${API_BASE}${endpoint}`;
